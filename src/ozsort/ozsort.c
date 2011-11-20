6089aad2f6f0da307d3804500c6270ed132c826c
Coder4 <liheyuan.bj@gmail.com>
2011-11-20 13:51:42 +0800

    Code review.

--- src/ozsort/ozsort.c
+++ src/ozsort/ozsort.c
@@ -30,33 +30,31 @@ int ozsort_split(OZSort* param)
     /* Global Var */
     FILE* fpsrc = NULL;
     FILE* fp = NULL;
-    long cnt;
-    long value;
-    char key[OZ_KEY_MAX];
     char fn[OZ_BUF_SIZE];
+    char key[OZ_KEY_MAX];
     int i;
+    long cnt;
     int ret;
-
+	
+	/* open original file */
     fpsrc = fopen(param->_src, "r");
     if(!fpsrc)
     {
         return 1;
     }
 
-    /* sort split one by one */
+    /* print base file name */
+    sprintf(fn, "/tmp/ozsort_%d_", time(NULL));
     param->_nfiles = 0;
     cnt = 0;
 
-    /* print base */
-    sprintf(fn, "/tmp/ozsort_%d_", time(NULL));
-
     while(1)
     {
-        /* Read OZSORT_PER_LINE lines unless EOF */
-        cnt = 0;
-        for(i=0; i<OZSORT_PER_LINE; i++)
+        /* Read OZSORT_PER_LINE lines unless EOF or reach the split count */
+        for(i = 0, cnt = 0; i < OZSORT_PER_LINE; i++)
         {
             ret = fscanf(fpsrc, "%s\t%llu\t%u\n", key, &split[cnt]._offset, &split[cnt]._length);
+			/* only read 3 items is considered succ */
             if(ret==3)
             {
                 split[cnt]._key = malloc(sizeof(char) * (strlen(key) + 1));
@@ -69,7 +67,7 @@ int ozsort_split(OZSort* param)
             }
         }
 
-        //Make filename and open it
+        /* Make new split filename and open it */
         if(param->_nfiles>=OZSORT_MAX_SPLITS)
         {
             return 3;    
@@ -80,16 +78,18 @@ int ozsort_split(OZSort* param)
         {
             return 2;    
         }
-        //Sort/output
+        param->_nfiles++;
+
+        /* Sort & output */
         qsort(split, cnt, sizeof(OZRecord), cmp_oz_record);
         for(i=0; i<cnt; i++)
         {
             fprintf(fp, "%s\t%llu\t%u\n", split[i]._key, split[i]._offset, split[i]._length);
         }
-        param->_nfiles++;
         fclose(fp);
         fp = NULL;
-
+		
+		/* finish all lines */
         if(ret==EOF)
         {
             break;    
